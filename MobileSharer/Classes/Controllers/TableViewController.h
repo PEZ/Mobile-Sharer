c8548aa8e03cc18d74faba0cb40e86e3649e20c4
Peter Stromberg <pez@pezius.com>
2011-06-26 16:29:00 +0200

    v1.3 Autorotate supported.

---
+++ MobileSharer/Classes/Controllers/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyClass.h
+//  MobileSharer
+//
+//  Created by Peter Stromberg on 2011-06-26.
+//  Copyright 2011 Better Than Tomorrow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyClass : TTTableViewController {
+    
+}
+
+@end
---
+++ MobileSharer/Classes/Controllers/TableViewController.m
@@ -0,0 +1,14 @@
+//
+//  MyClass.m
+//  MobileSharer
+//
+//  Created by Peter Stromberg on 2011-06-26.
+//  Copyright 2011 Better Than Tomorrow. All rights reserved.
+//
+
+#import "MyClass.h"
+
+
+@implementation MyClass
+
+@end
